acd62590c391386d2c09e1707505554df07afe76
Sarah Hawthorne <mail@iamvishnu.net>
2020-01-13 11:51:24 -0500

    Race expansion
    Added non-core races, simplified race object.

--- script.js
+++ script.js
@@ -1,30 +1,46 @@
 var races = {
-	custom: {
-		modifiers: [0,0,0,0,0,0],
-		allowEdit: true
-	},
-	elf: {
-		modifiers: [0,2,-2,2,0,0],
-		allowEdit: false
-	},
-	dwarf: {
-		modifiers: [0,0,2,0,2,-2],
-		allowEdit: false
-	},
-	halfling: {
-		modifiers: [-2,2,0,0,0,2],
-		allowEdit: false
-	},
-	gnome: {
-		modifiers: [-2,0,2,0,0,2],
-		allowEdit: false
-	}
+	custom: [0,0,0,0,0,0],
+	elf: [0,2,-2,2,0,0],
+	dwarf: [0,0,2,0,2,-2],
+	halfling: [-2,2,0,0,0,2],
+	gnome: [-2,0,2,0,0,2],
+	aasimar: [0,0,0,0,2,2],
+    	tiefling: [0,2,0,2,0,-2],
+	catfolk: [0,2,0,0,-2,2],
+	changeling: [0,0,-2,0,2,2],
+	dhampir: [0,2,-2,0,0,2],
+	drow: [0,2,-2,0,0,2],
+	drownoble: [0,4,-2,2,2,2],
+	duergar: [0,0,2,0,2,-4],
+	fetchling: [0,2,0,0,-2,2],
+	gillman: [0,0,2,0,-2,2],
+	goblin: [-2,4,0,0,0,-2],
+	grippli: [-2,2,0,0,2,0],
+	hobgoblin: [0,2,2,0,0,0],
+	ifrit: [0,2,0,0,-2,2],
+	kitsune: [-2,2,0,0,0,2],
+	kobold: [-4,2,-2,0,0,0],
+	merfolk: [0,2,2,0,0,2],
+	nagaji: [2,0,0,-2,0,2],
+	orc: [4,0,0,-2,-2,-2],
+	oread: [2,0,0,0,2,-2],
+	ratfolk: [-2,2,0,2,0,0],
+	samsaran: [0,0,-2,2,2,0],
+	strix: [0,2,0,0,0,-2],
+	suli: [2,0,0,-2,0,2],
+	svirfneblin: [-2,2,0,0,2,-4],
+	sylph: [0,2,-2,2,0,0],
+	tengu: [0,2,-2,0,2,0],
+	undine: [-2,2,0,0,2,0],
+	vanara: [0,2,0,0,2,-2],
+	vishkanya: [0,2,0,0,-2,2],
+	wayang: [0,2,0,2,-2,0]
 };
 
 var abilities = ["STR","DEX","CON","INT","WIS","CHA"];
 
 function ApplyRacialMod() {
-	var raceName = document.getElementById("raceSelector").value;
+	var raceName = document.getElementById("raceSelector").value.replace(" ","").toLowerCase();
 	var nameFound = false;
 	for (var prop in races) {
 		if (prop + "" == raceName) {
@@ -37,8 +53,8 @@ function ApplyRacialMod() {
 	var race = races[raceName];
 	for (var i = 0; i < abilities.length; i++) {
 		var inputBox = document.getElementsByName("racemod")[i];
-		inputBox.value = race.modifiers[i];
-		inputBox.disabled = !race.allowEdit;
+		inputBox.value = race[i];
+		inputBox.disabled = raceName != "custom";
 	}
 	
 	CalculateTotalValues();
